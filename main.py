05b25f3c3c196f21725354036d832235f64b6aea
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-04-19 00:28:21 -0300

    feat: add row multiplication

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ matrix = [
 def row_switching(r1, r2):
     matrix[r1], matrix[r2] = matrix[r2], matrix[r1]
 
+def row_multiplication(row, k):
+    if k == 0:
+        return
+    for col in range(len(matrix[row])):
+        matrix[row][col] *= k
+
