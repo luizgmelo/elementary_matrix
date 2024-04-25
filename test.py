0d0d761abe12eb6177fb11f849bf9455a086f08e
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2024-04-25 15:57:21 -0300

    test: add test for row multiplication

--- test.py
+++ test.py
@@ -43,8 +43,37 @@ class TestElementaryMatrix(unittest.TestCase):
 
         
 
-    # def test_row_multiplication(self):
-    # 
+    def test_row_multiplication(self):
+        matrix = [
+            [2, 1, 3],
+            [4, 2, 2],
+            [2, 5, 3],
+        ]
+        """
+        L1 -> 8 * L1
+        [16,8,24]
+        [4,2,2]
+        [2,5,3]
+        """
+        row_multiplication(0, 8, matrix)
+        self.assertEqual(matrix[0], [16,8,24])
+        """
+        L2 -> 3 * L2
+        [16,8,24]
+        [12,6,6]
+        [2,5,3]
+        """
+        row_multiplication(1, 3, matrix)
+        self.assertEqual(matrix[1], [12,6,6])
+        """
+        L3 -> 7 * L3
+        [16,8,24]
+        [12,6,6]
+        [14,35,21]
+        """
+        row_multiplication(2, 7, matrix)
+        self.assertEqual(matrix[2], [14,35,21])
+
     # def test_row_addition(self):
 
 
